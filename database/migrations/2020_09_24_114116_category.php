c51d6f778dace3e46ff9e9369cef4badb1619c2e
gorkem-gultekin <gultekingorkem1@gmail.com>
2020-10-13 23:51:29 +0300

    ++

--- database/migrations/2020_09_24_114116_category.php
+++ database/migrations/2020_09_24_114116_category.php
@@ -16,6 +16,7 @@ class Category extends Migration
         Schema::create('category', function (Blueprint $table) {
             $table->increments('id');
             $table->string('name');
+            $table->string('check_name');
         });
     }
 
